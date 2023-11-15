65d34bd4128c9d620252fd498066ffed8c43bfb0
Martijn Kunstman <martijn@kunstman.nl>
2023-11-16 00:09:28 +0100

    loose p5

--- script.js
+++ script.js
@@ -1,9 +1,85 @@
+class SpatialHash {
+  constructor(w,h, gridSize) {
+    this.gridSize = gridSize;
+    this.hashTable = [];
+    this.horizontal = Math.ceil(w / gridSize);
+    this.vertical = Math.ceil(h / gridSize);
+    this.hashTable = Array.from({ length: this.vertical }, () =>
+      Array.from({ length: this.horizontal }, () => [])
+    );
+    this.occupiedCells = new Set();
+  }
+  clear() {
+    this.occupiedCells.forEach((cell) => {
+      let [a, b] = cell.split(",").map(Number);
+      this.hashTable[a][b] = [];
+    });
+    this.occupiedCells.clear();
+  }
+  insert(object) {
+    let xIndex = Math.floor(object.x / this.gridSize);
+    let yIndex = Math.floor(object.y / this.gridSize);
+    this.hashTable[yIndex][xIndex].push(object);
+    this.occupiedCells.add(`${yIndex},${xIndex}`);
+  }
+}
+class Boid {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.direction = { x: Math.random() - 0.5, y: Math.random() - 0.5 };
+    this.w = CANVAS_WIDTH;
+    this.h = CANVAS_HEIGHT;
+  }
+  checkOverlay(parents) {
+    parents = [].concat(...parents);
+    for (let a = 0; a < parents.length; a++) {
+      counter++;
+      let disx = parents[a].x - this.x;
+      let disy = parents[a].y - this.y;
+      if (disx != 0 && disy != 0) {
+        if (Math.hypot(disx, disy) < config.diameter) {
+          fill(255, 0, 0);
+          if (config.useBreak) {
+            break;
+          }
+        }
+      }
+    }
+  }
+  update() {
+    // Cache properties for quicker access
+    let dx = this.direction.x;
+    let dy = this.direction.y;
+    let w = this.w;
+    let h = this.h;
+    // Update positions
+    this.x += dx;
+    this.y += dy;
+    // Handle horizontal boundaries
+    if (this.x < config.diameter || this.x > w - config.diameter) {
+      this.direction.x = -dx;
+      this.x = this.x < config.diameter ? config.diameter : w - config.diameter;
+    }
+    // Handle vertical boundaries
+    if (this.y < config.diameter || this.y > h - config.diameter) {
+      this.direction.y = -dy;
+      this.y = this.y < config.diameter ? config.diameter : h - config.diameter;
+    }
+  }
+}
+
 // Constants for canvas dimensions
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 500;
 const MAX_BOIDS = 2000;
 const MIN_DIAMETER = 5;
 const MAX_DIAMETER = 50;
+
+// Simulation setup
+let boids = [];
+let spacialHash;
+let grid;
 
 // GUI Configuration
 const config = {
@@ -20,15 +96,68 @@ gui.add(config, "useBreak").onChange(reinitialize);
 gui.add(config, "boidsCount", 10, MAX_BOIDS).step(1).onChange(reinitialize);
 gui.add(config, "diameter", MIN_DIAMETER, MAX_DIAMETER).step(1).onChange(reinitialize);
 
-// Simulation setup
-let boids = [];
-let spacialHash;
-let grid;
+// Create and insert the canvas element
+const canvas = document.createElement('canvas');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+document.body.appendChild(canvas);
 
-function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  reinitialize();
+// Get the canvas context for drawing
+const ctx = canvas.getContext('2d');
+
+// Replace p5's 'random' function
+function random(min, max) {
+  if (typeof max === 'undefined') {
+    max = min;
+    min = 0;
+  }
+  return Math.random() * (max - min) + min;
 }
+
+// Replace drawing functions with canvas context methods
+function fill(r, g, b) {
+  const color = g === undefined ? `rgb(${r}, ${r}, ${r})` : `rgb(${r}, ${g}, ${b})`;
+  ctx.fillStyle = color;
+}
+
+function rect(x, y, w, h) {
+  ctx.fillRect(x, y, w, h);
+}
+
+function circle(x, y, diameter) {
+  ctx.beginPath();
+  ctx.arc(x, y, diameter / 2, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function textSize(size) {
+  ctx.font = `${size}px sans-serif`;
+}
+
+function text(str, x, y) {
+  ctx.fillText(str, x, y);
+}
+
+// Replace 'frameRate()' with custom calculation
+let lastFrameTime = Date.now();
+function frameRate() {
+  const currentFrameTime = Date.now();
+  const fps = 1000 / (currentFrameTime - lastFrameTime);
+  lastFrameTime = currentFrameTime;
+  return fps;
+}
+
+// Mouse position tracking
+let mouseX = 0;
+let mouseY = 0;
+canvas.addEventListener('mousemove', (event) => {
+  mouseX = event.offsetX;
+  mouseY = event.offsetY;
+});
+
+// Start the simulation
+reinitialize();
+draw();
 
 function reinitialize() {
   boids = [];
@@ -70,7 +199,7 @@ function draw() {
     boids[a].update();
     spacialHash.insert(boids[a]);
   }
-  background(120, 120, 120);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   let xx = Math.floor(mouseX / grid);
   let yy = Math.floor(mouseY / grid);
@@ -118,76 +247,5 @@ function draw() {
   fill(0, 0, 0);
   textSize(16);
   text("fps:"+Math.round(frameRate()) + "- calc:" + counter, 10, 20);
-}
-
-class Boid {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.direction = { x: Math.random() - 0.5, y: Math.random() - 0.5 };
-    this.w = CANVAS_WIDTH;
-    this.h = CANVAS_HEIGHT;
-  }
-  checkOverlay(parents) {
-    parents = [].concat(...parents);
-    for (let a = 0; a < parents.length; a++) {
-      counter++;
-      let disx = parents[a].x - this.x;
-      let disy = parents[a].y - this.y;
-      if (disx != 0 && disy != 0) {
-        if (Math.hypot(disx, disy) < config.diameter) {
-          fill(255, 0, 0);
-          if (config.useBreak) {
-            break;
-          }
-        }
-      }
-    }
-  }
-  update() {
-    // Cache properties for quicker access
-    let dx = this.direction.x;
-    let dy = this.direction.y;
-    let w = this.w;
-    let h = this.h;
-    // Update positions
-    this.x += dx;
-    this.y += dy;
-    // Handle horizontal boundaries
-    if (this.x < config.diameter || this.x > w - config.diameter) {
-      this.direction.x = -dx;
-      this.x = this.x < config.diameter ? config.diameter : w - config.diameter;
-    }
-    // Handle vertical boundaries
-    if (this.y < config.diameter || this.y > h - config.diameter) {
-      this.direction.y = -dy;
-      this.y = this.y < config.diameter ? config.diameter : h - config.diameter;
-    }
-  }
-}
-
-class SpatialHash {
-  constructor(w,h, gridSize) {
-    this.gridSize = gridSize;
-    this.hashTable = [];
-    this.horizontal = Math.ceil(w / gridSize);
-    this.vertical = Math.ceil(h / gridSize);
-    this.hashTable = Array.from({ length: this.vertical }, () =>
-      Array.from({ length: this.horizontal }, () => [])
-    );
-    this.occupiedCells = new Set();
-  }
-  clear() {
-    this.occupiedCells.forEach((cell) => {
-      let [a, b] = cell.split(",").map(Number);
-      this.hashTable[a][b] = [];
-    });
-    this.occupiedCells.clear();
-  }
-  insert(object) {
-    let xIndex = Math.floor(object.x / this.gridSize);
-    let yIndex = Math.floor(object.y / this.gridSize);
-    this.hashTable[yIndex][xIndex].push(object);
-    this.occupiedCells.add(`${yIndex},${xIndex}`);
-  }
+  requestAnimationFrame(draw);
 }
